7c491e8eecd290df43bcda099d43f8f3abb132ba
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 05:31:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,13 @@
 def roll_call_dwarves(array)
   call = {}
   array.each_with_index{|item,index|
-  call[index] = item
+  call[item] = index}
+  call
   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map{|n| n.Capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
