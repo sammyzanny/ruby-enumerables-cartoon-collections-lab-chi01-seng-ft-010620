b817d88aa555036c87ff8883bfc5ae35f1d83161
Samuel Zandi <samuelczandi@gmail.com>
2020-01-03 05:45:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array)
   array.any?{|call|.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|n| cheese_types.any?
 end
